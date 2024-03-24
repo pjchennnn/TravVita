b20189b6cb392265c30f6ef932a16b1459ae0c6b
HowardYuuu <asportboy235@gmail.com>
2024-03-24 22:02:09 +0800

    3/24  修改後台bug  生成折扣碼仍有問題

--- prjTravelPlatformV3/Areas/Employee/Controllers/Discount/DApiCController.cs
+++ prjTravelPlatformV3/Areas/Employee/Controllers/Discount/DApiCController.cs
@@ -26,7 +26,7 @@ namespace prjTravelPlatformV3.Areas.Employee.Controllers.Discount
                              fCouponCode = c.折扣碼,
                              fCouponName = c.名稱,
                              //fAmount = "$" + Convert.ToInt32(c.使用金額),
-                             fDiscount = "$" +Convert.ToInt32(c.折扣值) ,
+                             fDiscount = "$" + Convert.ToInt32(c.折扣值),
                              fStartDate = Convert.ToDateTime(c.啟用日期).ToString("d"),
                              fEndDate = Convert.ToDateTime(c.截止日期).ToString("d"),
                              fProductType = c.商品類型,
@@ -52,90 +52,90 @@ namespace prjTravelPlatformV3.Areas.Employee.Controllers.Discount
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(CouponViewModel coupon)
         {
-            if (ModelState.IsValid)
+            //if (ModelState.IsValid)
+            //{
+            try
             {
-                try
+                TDcCouponList tCoupon = new TDcCouponList
                 {
-					TDcCouponList tCoupon = new TDcCouponList
-					{
-                        FCouponId = coupon.CouponId,
-                        FCouponCode = coupon.CouponCode,
-                        FCouponName = coupon.CouponName,
-                        FAmount = coupon.Amount,
-                        FDiscount = coupon.Discount,
-                        FStartDate = coupon.StartDate,
-                        FEndDate = coupon.EndDate,
-                        FProductType = coupon.ProductType,
-                            FRule = coupon.Rule,
-                            //FNote = coupon.Note,
-                            FEnable = coupon.Enable,
-                    };
-                    _context.Add(tCoupon);
-                    await _context.SaveChangesAsync();
-                    return Json(new { success = true, message = "資料新增成功" });
-                }
-                catch (Exception e)
-                {
-                    return Json(new { success = false, message = $"資料新增失敗：{e.Message}" });
-                }
+                    FCouponId = coupon.CouponId,
+                    FCouponCode = coupon.CouponCode,
+                    FCouponName = coupon.CouponName,
+                    FAmount = coupon.Amount,
+                    FDiscount = coupon.Discount,
+                    FStartDate = coupon.StartDate,
+                    FEndDate = coupon.EndDate,
+                    FProductType = coupon.ProductType,
+                    FRule = coupon.Rule,
+                    //FNote = coupon.Note,
+                    FEnable = coupon.Enable,
+                };
+                _context.Add(tCoupon);
+                await _context.SaveChangesAsync();
+                return Json(new { success = true, message = "資料新增成功" });
             }
-            //驗證沒過            
-            var errors = ModelState.ToDictionary(
-                kvp => kvp.Key,
-                kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToList()
-            );
-            //var errors = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
-            return Json(new
+            catch (Exception e)
             {
-                success = false,
-                message = "資料驗證失敗",
-                errors
-            });
+                return Json(new { success = false, message = $"資料新增失敗：{e.Message}" });
+            }
+            //}
+            ////驗證沒過            
+            //var errors = ModelState.ToDictionary(
+            //    kvp => kvp.Key,
+            //    kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToList()
+            //);
+            ////var errors = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
+            //return Json(new
+            //{
+            //    success = false,
+            //    message = "資料驗證失敗",
+            //    errors
+            //});
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(CouponViewModel coupon)
         {
-            if (ModelState.IsValid)
+            //if (ModelState.IsValid)
+            //{
+            try
             {
-                try
+                TDcCouponList tCoupon = new TDcCouponList
                 {
-					TDcCouponList tCoupon = new TDcCouponList
-					{
-                        FCouponId = coupon.CouponId,
-                        FCouponCode = coupon.CouponCode,
-                        FCouponName = coupon.CouponName,
-                        FAmount = coupon.Amount,
-                        FDiscount = coupon.Discount,
-                        FStartDate = coupon.StartDate,
-                        FEndDate = coupon.EndDate,
-                        FProductType = coupon.ProductType,
-                        FRule = coupon.Rule,
-                        //FNote = coupon.Note,
-                        FEnable = coupon.Enable,
-                    };
-                    _context.Update(tCoupon);
-                    await _context.SaveChangesAsync();
-                    return Json(new { success = true, message = "資料修改成功" });
-                }
-                catch (Exception e)
-                {
-                    return Json(new { success = false, message = $"資料修改失敗：{e.Message}" });
-                }
+                    FCouponId = coupon.CouponId,
+                    FCouponCode = coupon.CouponCode,
+                    FCouponName = coupon.CouponName,
+                    FAmount = coupon.Amount,
+                    FDiscount = coupon.Discount,
+                    FStartDate = coupon.StartDate,
+                    FEndDate = coupon.EndDate,
+                    FProductType = coupon.ProductType,
+                    FRule = coupon.Rule,
+                    //FNote = coupon.Note,
+                    FEnable = coupon.Enable,
+                };
+                _context.Update(tCoupon);
+                await _context.SaveChangesAsync();
+                return Json(new { success = true, message = "資料修改成功" });
             }
-            //驗證沒過            
-            var errors = ModelState.ToDictionary(
-                kvp => kvp.Key,
-                kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToList()
-            );
-            //var errors = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
-            return Json(new
+            catch (Exception e)
             {
-                success = false,
-                message = "資料驗證失敗",
-                errors
-            });
+                return Json(new { success = false, message = $"資料修改失敗：{e.Message}" });
+            }
+            //}
+            ////驗證沒過            
+            //var errors = ModelState.ToDictionary(
+            //    kvp => kvp.Key,
+            //    kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToList()
+            //);
+            ////var errors = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
+            //return Json(new
+            //{
+            //    success = false,
+            //    message = "資料驗證失敗",
+            //    errors
+            //});
         }
         #endregion
     }
---
+++ prjTravelPlatformV3/wwwroot/js/Discount/CouponCode.js
@@ -0,0 +1,26 @@
+﻿console.log("作用正常");
+// 監聽 checkbox 的變更事件
+document.getElementById("autoGenerateCheckbox").addEventListener("change", function () {
+    var checkbox = this;
+    var couponCodeInput = document.getElementById("couponCode");
+    console.log("作用正常");
+    if (checkbox.checked) {
+        // 生成十碼折扣碼
+        var discountCode = generateDiscountCode(10);
+        // 將折扣碼填充到 input 元素中
+        couponCodeInput.value = discountCode;
+    } else {
+        // 清空 input 元素
+        couponCodeInput.value = '';
+    }
+});
+
+// 生成指定長度的折扣碼
+function generateDiscountCode(length) {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var discountCode = '';
+    for (var i = 0; i < length; i++) {
+        discountCode += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return discountCode;
+};
